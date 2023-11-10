370bf62b401b026465464eb65a2aa05058dabab1
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-10 17:36:27 +0100

    definito il lavoro tramite commenti su js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/* richiedere all'utente l'età del passeggero */
+/* richiedere all'utente il numero di km da percorrere */
+
+/* colcolare il prezzo totale del viaggio */
+    /*? calcolare prezzo viaggio in base ai km */
+    /*? applicare sconto del 20% se <18 anni */
+    /*? applicare sconto del 40% se >65 anni */
+
+/* mostrare sul schermo il prezzo eventualmente scontato con soli 2 decimali */
