0819b122fd544bb37b1fceca3f8ca53bc1b602e1
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-10 17:51:49 +0100

    inserita funzione travelPrice (km * 0.21)

--- js/script.js
+++ js/script.js
@@ -2,13 +2,19 @@
 const passengerAge = parseInt(prompt('Inserire età del Passeggero'));
 
 /* richiedere all'utente il numero di km da percorrere */
-const travelDistance = parseInt(prompt('Inserire chilometri del viaggio'))
+const travelDistance = parseInt(prompt('Inserire il numero di Chilometri del viaggio'))
 
 
 console.log(`età del passeggero: ${passengerAge} - lunghezza del tragitto: ${travelDistance} `)
+
+/*? calcolare prezzo viaggio in base ai km */
+
+let travelPrice = travelDistance * 0.21
+
+console.log(`Prezzo del viaggio senza sconti: ${travelPrice}€`)
+
+/*? applicare sconto del 20% se <18 anni */
+/*? applicare sconto del 40% se >65 anni */
 /* colcolare il prezzo totale del viaggio */
-    /*? calcolare prezzo viaggio in base ai km */
-    /*? applicare sconto del 20% se <18 anni */
-    /*? applicare sconto del 40% se >65 anni */
 
 /* mostrare sul schermo il prezzo eventualmente scontato con soli 2 decimali */
